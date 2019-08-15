b8438eb43d31a91e0de52421d9d80473c8fa20a5
ElMariachiCraZy <34199778+ElMariachiCraZy@users.noreply.github.com>
2019-08-14 21:13:34 -0500

    GeolocalizacionRutas

--- src/app/pages/tab-gps/tab-gps.page.ts
+++ src/app/pages/tab-gps/tab-gps.page.ts
@@ -19,15 +19,11 @@ declare var google;
   templateUrl: "./tab-gps.page.html",
   styleUrls: ["./tab-gps.page.scss"]
 })
-export class TabGpsPage implements OnInit, AfterViewInit {
+export class TabGpsPage implements OnInit {
   @ViewChild("mapElement", { static: false }) mapNativeElement: ElementRef;
+  @ViewChild("end", { static: false }) endElement: ElementRef;
   directionsService = new google.maps.DirectionsService();
-  directionsDisplay = new google.maps.DirectionsRenderer({
-    suppressMarkers: true,
-    polylineOptions: {
-      strokeColor: "green"
-    }
-  });
+  directionsRenderer = new google.maps.DirectionsRenderer();
   directionForm: FormGroup;
   map;
   currentLocation: any = {
@@ -39,20 +35,51 @@ export class TabGpsPage implements OnInit, AfterViewInit {
     text: ""
   };
 
-  constructor(private fb: FormBuilder, private geolocation: Geolocation) {
-    this.createDirectionForm();
-  }
+  pos: any = {
+    lat: 0,
+    lng: 0
+  };
 
-  ngOnInit() {}
+  constructor(private fb: FormBuilder, private geolocation: Geolocation) {}
 
-  createDirectionForm() {
-    this.directionForm = this.fb.group({
-      destination: ["", Validators.required]
-    });
-  }
-
-  ngAfterViewInit(): void {
+  ngOnInit() {
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsRenderer = new google.maps.DirectionsRenderer();
     this.geolocation.getCurrentPosition().then(resp => {
+      this.currentLocation.lat = resp.coords.latitude;
+      this.currentLocation.lng = resp.coords.longitude;
+
+      this.map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 7,
+        center: { lat: 41.85, lng: -87.65 }
+      });
+      this.map.setCenter(this.currentLocation);
+      this.directionsRenderer.setMap(this.map);
+    });
+    var t = this;
+    var onChangeHandler = function() {};
+  }
+
+  calculateAndDisplayRoute(destination): void {
+    var t = this;
+    this.directionsService.route(
+      {
+        origin: t.currentLocation,
+        destination: t.destination.text,
+        provideRouteAlternatives: true,
+        travelMode: "DRIVING"
+      },
+      (response, status) => {
+        if (status === "OK") {
+          this.directionsRenderer.setDirections(response);
+        } else {
+          window.alert("Directions request failed due to " + status);
+        }
+      }
+    );
+  }
+
+  /*this.geolocation.getCurrentPosition().then(resp => {
       this.currentLocation.lat = resp.coords.latitude;
       this.currentLocation.lng = resp.coords.longitude;
 
@@ -65,16 +92,36 @@ export class TabGpsPage implements OnInit, AfterViewInit {
         lat: this.currentLocation.lat,
         lng: this.currentLocation.lng
       };
-      infoWindow.setPosition(pos);
+      /*infoWindow.setPosition(pos);
       infoWindow.setContent("Location found.");
       infoWindow.open(this.map);
       this.map.setCenter(pos);
     });
 
     this.directionsDisplay.setMap(this.map);
-  }
-
-  calculateAndDisplayRoute(formValues) {
+  }*/
+  /* calculateAndDisplayRoute(formValues) {
+    let directionsDisplay = new google.maps.DirectionsRenderer();
+    const _self = this;
+    this.directionsService.route(
+      {
+        origin: this.currentLocation,
+        destination: this.destination.text,
+        travelMode: "DRIVING"
+      },
+      (response, status) => {
+        if (status === "OK") {
+          try {
+            directionsDisplay.setDirections(response);
+            console.log(response);
+          } catch (e) {
+            console.log(e);
+          }
+        } else {
+          window.alert("Directions request failed due to " + status);
+        }
+      }
+    );
     /*var request1 = {
       origin: this.currentLocation,
       destination: this.destination.text,
@@ -86,12 +133,12 @@ export class TabGpsPage implements OnInit, AfterViewInit {
         t.directionsDisplay.setDirections(response);
        }
       })*/
-    this.directionsService.route(
+  /*this.directionsService.route(
       {
         origin: this.currentLocation,
         destination: this.destination.text,
         travelMode: google.maps.DirectionsTravelMode.DRIVING,
-        provideRouteAlternatives: true
+        provideRouteAlternatives: true,
       },
       (response, status) => {
         if (status === "OK") {
@@ -101,6 +148,19 @@ export class TabGpsPage implements OnInit, AfterViewInit {
           window.alert("Directions request failed due to " + status);
         }
       }
-    );
-  }
+    );*/
+  /*var request = {
+      origin: {query: this.currentLocation},
+      destination: {query: this.destination.text},
+      travelMode: "DRIVING"
+    };
+    this.directionsService.route(request, function(result, status) {
+      if (status == "OK") {
+        var m = new google.maps.DirectionsRenderer().setDirections(result);
+        console.log(m);
+        //var display = directionsDisplay.setDirections(result);
+      }else{
+        window.alert('Directions request failed due to ' + status);
+      }
+    });*/
 }
