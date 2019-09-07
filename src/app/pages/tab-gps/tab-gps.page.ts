458f10035599bd2f8c0aae809a81ff3bace91dee
ElMariachiCraZy <34199778+ElMariachiCraZy@users.noreply.github.com>
2019-09-07 18:02:42 -0500

    Videos

--- src/app/pages/tab-gps/tab-gps.page.ts
+++ src/app/pages/tab-gps/tab-gps.page.ts
@@ -1,3 +1,4 @@
+import { Platform } from "@ionic/angular";
 import {
   AfterViewInit,
   Component,
@@ -13,6 +14,8 @@ import {
 } from "@angular/forms";
 
 import { Geolocation, Geoposition } from "@ionic-native/geolocation/ngx";
+import { AndroidPermissions } from "@ionic-native/android-permissions/ngx";
+import { LocationAccuracy } from "@ionic-native/location-accuracy/ngx";
 
 declare var google;
 
@@ -43,10 +46,80 @@ export class TabGpsPage implements OnInit {
     lng: 0
   };
 
-  constructor(private fb: FormBuilder, private geolocation: Geolocation) {}
+  constructor(
+    private fb: FormBuilder,
+    private geolocation: Geolocation,
+    public platform: Platform,
+    public androidPermissions: AndroidPermissions,
+    public locationAccuracy: LocationAccuracy
+  ) {}
 
   ngOnInit() {
     this.loadMap();
+    this.androidPermissions
+      .checkPermission(
+        this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION
+      )
+      .then(
+        result => {
+          if (result.hasPermission) {
+            //If having permission show 'Turn On GPS' dialogue
+            this.askToTurnOnGPS();
+          } else {
+            //If not having permission ask for permission
+            this.requestGPSPermission();
+          }
+        },
+        err => {
+          console.log(err);
+          alert(err);
+        }
+      );
+  }
+
+  requestGPSPermission() {
+    this.locationAccuracy.canRequest().then((canRequest: boolean) => {
+      if (canRequest) {
+        console.log("4");
+      } else {
+        //Show 'GPS Permission Request' dialogue
+        this.androidPermissions
+          .requestPermission(
+            this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION
+          )
+          .then(
+            () => {
+              // call method to turn on GPS
+              this.askToTurnOnGPS();
+            },
+            error => {
+              console.log(error);
+              //Show alert if user click on 'No Thanks'
+              alert(
+                "requestPermission Error requesting location permissions " +
+                  error
+              );
+            }
+          );
+      }
+    });
+  }
+
+  askToTurnOnGPS() {
+    this.locationAccuracy
+      .request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY)
+      .then(
+        () => {
+          // When GPS Turned ON call method to get Accurate location coordinates
+          this.getPosition();
+        },
+        error => {
+          console.log(error);
+          alert(
+            "Error requesting location permissions " + JSON.stringify(error)
+          );
+        }
+      );
   }
 
   loadMap() {
@@ -56,15 +129,21 @@ export class TabGpsPage implements OnInit {
         center: { lat: 41.85, lng: -87.65 }
       });
     } catch (e) {
-      window.alert("No CurrentLocation " + e);
+      window.alert("Map load failed" + e);
     }
     this.directionsRenderer.setMap(this.map);
   }
 
   getPosition(): any {
-    this.geolocation.getCurrentPosition().then(resp => {
-      this.setCenter(resp);
-    });
+    this.geolocation
+      .getCurrentPosition()
+      .then(resp => {
+        this.setCenter(resp);
+        console.log(resp);
+      })
+      .catch(err => {
+        console.log("error obteniendo location " + err);
+      });
   }
 
   setCenter(position: Geoposition) {
--- src/app/pages/tab-terapias/tab-terapias.page.ts
+++ src/app/pages/tab-terapias/tab-terapias.page.ts
@@ -1,33 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer} from '@angular/platform-browser';
+import { Component, OnInit } from "@angular/core";
+import { YoutubeVideoPlayer } from "@ionic-native/youtube-video-player/ngx";
 
 @Component({
-  selector: 'app-tab-terapias',
-  templateUrl: './tab-terapias.page.html',
-  styleUrls: ['./tab-terapias.page.scss'],
+  selector: "app-tab-terapias",
+  templateUrl: "./tab-terapias.page.html",
+  styleUrls: ["./tab-terapias.page.scss"]
 })
 export class TabTerapiasPage implements OnInit {
-
-  vid = 'https://www.youtube.com/embed/LOe_wcdmZ38';
-  url = '';
+  vid = "https://www.youtube.com/embed/LOe_wcdmZ38";
+  url = "";
   videos = [
     {
-      title: 'Terapia 1',
-      url: 'https://www.youtube.com/embed/LOe_wcdmZ38'
+      title: "Terapia 1",
+      url: "https://www.youtube.com/embed/LOe_wcdmZ38"
     },
     {
-      title: 'Terapia 2',
-      url: 'https://www.youtube.com/embed/LOe_wcdmZ38'
+      title: "Terapia 2",
+      url: "https://www.youtube.com/embed/LOe_wcdmZ38"
     }
   ];
-  constructor(private dom: DomSanitizer) {
+  constructor(private youtube: YoutubeVideoPlayer) {}
 
-  }
-  sanitizer(vid) {
-    return this.dom.bypassSecurityTrustResourceUrl(vid);
-  }
-
-  ngOnInit() {
+  onClick1(id) {
+    this.youtube.openVideo(id);
   }
 
+  onClick2(id) {
+    this.youtube.openVideo(id);
+  }
+
+  onClick3(id) {
+    this.youtube.openVideo(id);
+  }
+
+  onClick4(id) {
+    this.youtube.openVideo(id);
+  }
+
+  ngOnInit() {}
 }
