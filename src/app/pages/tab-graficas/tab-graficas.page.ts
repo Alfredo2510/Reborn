6fe8ddfe8d7917f018cbd60659d76a6391a1d72d
Raymond Calderon <raymond.cm21@gmail.com>
2019-09-19 17:18:57 -0500

    Cambio de graficar a buscar

--- src/app/pages/tab-graficas/tab-graficas.page.ts
+++ src/app/pages/tab-graficas/tab-graficas.page.ts
@@ -7,43 +7,43 @@ import { Chart } from 'chart.js';
   styleUrls: ['./tab-graficas.page.scss'],
 })
 export class TabGraficasPage implements OnInit {
-  @ViewChild('barCanvas', {static: true}) barCanvas: ElementRef;
+  // @ViewChild('barCanvas', {static: true}) barCanvas: ElementRef;
 
-  private barChart: Chart;
+  // private barChart: Chart;
 
   constructor() {}
 
   ngOnInit() {
-    this.barChart = new Chart(this.barCanvas.nativeElement, {
-      type: 'bar',
-      data: {
-        labels: ['Traumatismos Superficiales', 'Luxaciones, esguinces y desgarros', 'Heridas', 'Fracturas', 'Traumatimos', 'Dorsalgias', 'Quemaduras', 'Cuerpo Extraño', 'Amputaciones', 'Varios de frecuencia menor'],
-        datasets: [{
-          label: 'Hombres',
-          data: [64652, 59292, 57363, 28487, 15316, 4862, 6036, 4753, 3269, 6496],
-          backgroundColor: '#ddee44', // array should have same number of elements as number of dataset
-          borderColor: '#ddee44', // array should have same number of elements as number of dataset
-          borderWidth: 1
-        },
-        {
-          label: 'Mujeres',
-          data: [29249, 29122, 14029, 4975, 4885, 1909, 2221, 669, 556, 2560],
-          backgroundColor: '#dd1144', // array should have same number of elements as number of dataset
-          borderColor: '#dd1144', // array should have same number of elements as number of dataset
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true
-              }
-            }
-          ]
-        }
-      }
-    });
+    // this.barChart = new Chart(this.barCanvas.nativeElement, {
+    //   type: 'bar',
+    //   data: {
+    //     labels: ['Traumatismos Superficiales', 'Luxaciones, esguinces y desgarros', 'Heridas', 'Fracturas', 'Traumatimos', 'Dorsalgias', 'Quemaduras', 'Cuerpo Extraño', 'Amputaciones', 'Varios de frecuencia menor'],
+    //     datasets: [{
+    //       label: 'Hombres',
+    //       data: [64652, 59292, 57363, 28487, 15316, 4862, 6036, 4753, 3269, 6496],
+    //       backgroundColor: '#ddee44', // array should have same number of elements as number of dataset
+    //       borderColor: '#ddee44', // array should have same number of elements as number of dataset
+    //       borderWidth: 1
+    //     },
+    //     {
+    //       label: 'Mujeres',
+    //       data: [29249, 29122, 14029, 4975, 4885, 1909, 2221, 669, 556, 2560],
+    //       backgroundColor: '#dd1144', // array should have same number of elements as number of dataset
+    //       borderColor: '#dd1144', // array should have same number of elements as number of dataset
+    //       borderWidth: 1
+    //     }]
+    //   },
+    //   options: {
+    //     scales: {
+    //       yAxes: [
+    //         {
+    //           ticks: {
+    //             beginAtZero: true
+    //           }
+    //         }
+    //       ]
+    //     }
+    //   }
+    // });
   }
 }
